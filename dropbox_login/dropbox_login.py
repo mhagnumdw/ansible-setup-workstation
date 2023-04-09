f948cd7aea1e62e31f5b65700c36b17f14484530
Dwouglas Mhagnum <dwouglas@gmail.com>
2023-04-09 19:49:26 -0300

    WIP: dropbox login: vai iniciar a conf para epecificar o quê sincronizar

--- dropbox_login/dropbox_login.py
+++ dropbox_login/dropbox_login.py
@@ -1,6 +1,8 @@
 import time
 import argparse
 
+from selenium.common.exceptions import WebDriverException
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
@@ -43,6 +45,7 @@ print("Obtendo os inputs de username")
 elements_username = driver.find_elements(By.NAME, 'login_email')
 print(f"Total: {len(elements_username)}")
 
+# por alguma viagem, o dropbox tem mais de um elemento de input de username
 for element in elements_username:
     if element.is_enabled() and element.is_displayed():
         print(element)
@@ -52,25 +55,48 @@ print("Obtendo os inputs de password")
 elements_password = driver.find_elements(By.NAME, 'login_password')
 print(f"Total: {len(elements_password)}")
 
+# por alguma viagem, o dropbox tem mais de um elemento de input de password
 for element in elements_password:
     if element.is_enabled() and element.is_displayed():
         print(element)
         element.send_keys(args.password)
 
-print("Obtendo os botões de submit")
-elements_button_submit = driver.find_elements(By.CSS_SELECTOR, 'button.login-button[type="submit"] div.signin-text')
-print(f"Total: {len(elements_button_submit)}")
+# print("Obtendo os botões de submit")
+# elements_button_submit = driver.find_elements(By.CSS_SELECTOR, 'button.login-button[type="submit"] div.signin-text')
+# print(f"Total: {len(elements_button_submit)}")
 
-# // TODO: descomentar
 # for element in elements_button_submit:
 #     if element.is_enabled() and element.is_displayed():
 #         print(element)
 #         element.click()
 
-# // TODO: o Dropbox manda um email com um código, que é preciso digitar
-# na página. E aí ?! Talvez esse passo tenha que ser manual.
+body_element = driver.find_element(By.TAG_NAME, 'body')
 
-print("Esperando 60 segundos")
-time.sleep(60)
+script = """
+    var modal = document.createElement('div');
+    modal.innerHTML = '<h1 style="font-weight:bold; color:black;">Prossiga Manualmente</h1><p style="color:black;">O formulário abaixo já deve estar preenchido com o usuário e senha. Clique no botão Entrar/Sign in. O Dropbox envia um código para o email. Pegue o código, digite no campo abaixo e confirme.</p>';
+    modal.style.position = 'fixed';
+    modal.style.top = '0%';
+    modal.style.left = '50%';
+    modal.style.display = 'block';
+    modal.style.transform = 'translate(-50%, -0)';
+    modal.style.backgroundColor = 'yellow';
+    modal.style.zIndex = '9999';
+    modal.style.padding = '5px';
+    document.getElementsByTagName('body')[0].prepend(modal);
+"""
+
+# Adiciona um modal com instruções para o usuário
+driver.execute_script(script)
+
+# Espera o usuário fechar o browser para encerrar o script
+while True:
+    try:
+        driver.title
+    except WebDriverException:
+        break
+
+# print("Esperando 60 segundos")
+# time.sleep(60)
 
 # driver.quit()
